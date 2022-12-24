8130cc7ea98b6887da874e09361a91dae7f7da98
parkjs <python2_7@naver.com>
2022-12-24 22:49:31 +0900

    221224_메인화면_피드 관련 Column crossAxisAlignment 속성 변경(center -> start)

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.network(
             "https://cdn2.thecatapi.com/images/kat_7kqBi.png",
@@ -57,33 +58,51 @@ class HomePage extends StatelessWidget {
           Row(
             children: [
               IconButton(
-                icon: const Icon(
-                  CupertinoIcons.heart
-                ),
-                onPressed: (){
-
-                },
+                icon: const Icon(CupertinoIcons.heart),
+                onPressed: () {},
               ),
               IconButton(
-                icon: const Icon(
-                    CupertinoIcons.chat_bubble
-                ),
-                onPressed: (){
-
-                },
+                icon: const Icon(CupertinoIcons.chat_bubble),
+                onPressed: () {},
               ),
               //최대한 빈 공간을 만들어 주는 위젯
               const Spacer(),
               IconButton(
-                icon: const Icon(
-                    CupertinoIcons.book
-                ),
-                onPressed: (){
-
-                },
+                icon: const Icon(CupertinoIcons.book),
+                onPressed: () {},
               ),
             ],
-          )
+          ),
+
+          // 좋아요
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "2 likes",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // 설명
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute??",
+            ),
+          ),
+
+          // 날짜
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "FEBURARY 6",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
         ],
       ),
     );
